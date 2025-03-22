70585f2994a5546976e991da5426f021b599c753
JohnJohnsonTaren <61081845+JohnJohnsonTaren@users.noreply.github.com>
2025-03-22 13:14:14 +0200

    Add files via upload

---
+++ UserProcess.java
@@ -0,0 +1,39 @@
+package Task2;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserProcess {
+    public void processUsers(String inputFile, String outputFile) {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+            reader.readLine();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(" ");
+                if (parts.length == 2) {
+                    User user = new User(parts[0], Integer.parseInt(parts[1]));
+                    users.add(user);
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(users);
+
+        try (FileWriter writer = new FileWriter(outputFile)) {
+            writer.write(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
